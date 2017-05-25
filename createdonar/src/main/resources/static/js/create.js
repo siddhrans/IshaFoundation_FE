93aa4e756bee00c1883bdafd78b67d121ba26d93
Siddhrans <Siddhrans>
2017-05-25 18:10:13 +0530

    all files deleted

--- createdonar/src/main/resources/static/js/create.js
+++
@@ -1,8 +0,0 @@
-
-var app =  angular.module("myapp",[]).controller("myc",function($log,$scope){
-	$log.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-	$scope.submit = function(){
-		$log.log("#################################");
-	}
-})
-
