1fc67dad5d04e6c26b374719ad14e2736476904e
Meghanahs <meghasrinivas122@gmail.com>
2017-08-16 18:50:58 +0530

    complete Bulk donor module

--- IshaDonation_FE/src/main/resources/static/js/controller/controller.js
+++ IshaDonation_FE/src/main/resources/static/js/controller/controller.js
@@ -1,34 +1,36 @@
 'use strict';
-var myApp = angular.module("loginApp",[]);
-myApp.controller("loginController",['$log','$scope','localService','$http',function($log,$scope,localService,$httpa){
+var myApp = angular.module("loginApp",['ngRoute','ngResource','angularUtils.directives.dirPagination']);
+myApp.controller("loginController",['$log','$scope','localService','fileUpload','$http','$resource','$window',function($log,$scope,localService,fileUpload,$http,$resource,$window){
 	$log.log("------------Controller-----------");
 	
 	
 	var self = this;
-	self.registerData = {registrationFirstName:'',registrationLastName:'',registrationEmail:'',
-					registrationMobile:'',registrationPasscode:'',registrationConfirmPasscode:''};
+	self.registerData = {userName:'',name:'',email:'',
+			phoneNo:'',password:'',confirmPassword:''};
 	
-	self.donar_info={ consumerCode:'',donorfirstname:"",donorlastname:"",donoremail:"",donormobile:"",donorAddress:"",
-			donorRegion:"",donorCity:"",donorState:"",donorCenter:"",
-			bankDetailsAccountHolderName:"",bankDetailsBankName:'',bankDetailsBranchName:"",bankDetailsAccountNo:"",
-			bankDetailsIFSCCode:"",micr:"",bankDetailsAccountType:"",
-			paymentDetailsStartDate:"",paymentDetailsEndDate:"",paymentDetailsAmountInRs:"",
-			paymentDetailsFrequency:"",comments:"",createdonorstatus:''};
+	self.donor_info={donorName:"",donorPhoneNumber:"",donorEmail:"",donorRegion:"",donorState:"",donorCity:"",donorcenter:"",bankAccountholderName:"",
+	         bankName:"",bankBranch:"",bankAccountNumber:"",bankMICR:"",bankIfscCode:"",bankAccountType:"",donationStartDate:"",
+	         donationEndDate:"",donationFrequency:"",donorComments:"",amount:""};
 	
 	
 	
-	self.registerd = [];
+/*	self.registerd = [];*/
 	self.SuccessMessage='';
-
+	//self.data='';
+   // self.file={};
 	self.DonorsList = [];
 	self.successMessage = '';
     self.errorMessage = '';
 	self.onlyNumbers = /^\d+([,.]\d+)?$/;
 	
-	var url='http://localhost:6512/donar1/';
+	/*var url='http://localhost:6512/donor1/';*/
 	self.editUser = {};
-	
-	
+	self.logs={phoneNo:'',password:''};
+	self.filename='';
+	//self.selectedUploadFile;
+	var today = new Date();
+/*	self.logedUser=[];*/
+
 	/* drop down validation */	
 	$scope.type = [{
         id: "a",
@@ -45,7 +47,7 @@ myApp.controller("loginController",['$log','$scope','localService','$http',funct
     },{
 	    id: "e",
        value: "Other"
-}];
+    }];
 	
 	$scope.amountFrequency = [{
         id: "a",
@@ -68,32 +70,27 @@ myApp.controller("loginController",['$log','$scope','localService','$http',funct
     },{
         id: "g",
         value: "As And When Presented"
-}];
+    }];
  
 	
 	self.register = function register(){
 		$log.log("-----Register Function-----");
-		$log.log(self.registerData.registrationPasscode+" andd    "+self.registerData.registrationConfirmPasscode);
-		if(self.registerData.registrationPasscode == self.registerData.registrationConfirmPasscode){
-			localService.register(self.registerData)
+		if(self.registerData.password == self.registerData.confirmPassword){
+		localService.register(self.registerData)
 			.then(
 				function(d){
-					$log.log(self.registerData);
 					self.registerd = d;
 					self.SuccessMessage='New User Registerd SuccessFully';
-					alert("registered SuccessFully");
-					
+					window.location.href="http://localhost:8080/index.html#!/dashboard";
 					self.ErrorMessage='';
-					//$log.log(self.registerd);
 				},	
 				function(errResponse){
-					self.ErrorMessage='Error while Creating';
-					$log.log("--------Error in Controller----");
+					self.ErrorMessage='Error while Creating!Please try again later';
 				}
 			);
 		}else {
 			self.ErrorMessage="Passwords didn't match!! Please try again.";
-			/*window.location.href="http://localhost:6512/login.html";*/
+			window.location.href="http://localhost:8080/login.html";
 			$log.log("Error while Registering");
 		}
 		
@@ -107,12 +104,10 @@ myApp.controller("loginController",['$log','$scope','localService','$http',funct
 					$log.log("successfully fetched data");
 					self.DonorsList = fetchResponse;
 					self.OldDonors = self.DonorsList.length;
-					$log.log(self.OldDonors);
-					$log.log(self.DonorsList);
 			
 		},function(errResponse){
 			$log.log("Error while fetching");
-			alert("Error while fetching");
+			/*alert("Error while fetching");*/
 		})
 		
 	};
@@ -120,49 +115,167 @@ myApp.controller("loginController",['$log','$scope','localService','$http',funct
 	
 	
 	self.createDonor =  function createDonor() {
-         console.log('create donor');
-         
-         console.log(self.donar_info);
-         localService.createDonor(self.donar_info)
-             .then(
-                 function (response) {
-                     console.log('donor created successfully');
-                     self.successMessage = 'donor created successfully';
-                     self.errorMessage='';
-                     self.done = true;
-                  /*   self.user={};*/
-                 },
-                 function (errResponse) {
-                     console.error('Error while creating donor');
-                  /*   self.errorMessage = 'Error while creating donor:' + errResponse.data.errorMessage;
-                     self.successMessage='';*/
-                 }
-             );
+        console.log('create donor');
+       
+        console.log(self.donor_info);
+        localService.createDonor(self.donor_info)
+            .then(
+                function (response) {
+                    console.log('donor created successfully');
+                    self.successMessage = 'donor created successfully';
+                    alert('Donor Created successfully');
+                    window.location.href="#!SuccessPage"; 
+                    self.reload(); 
+                    self.errorMessage='';
+                    self.done = true;
+                 /*   self.user={};*/
+                },
+                function (errResponse) {
+                    console.error('Error while creating donor');
+                    alert('Error while creating Donor');
+                 
+                 /*   self.errorMessage = 'Error while creating donor:' + errResponse.data.errorMessage;
+                    self.successMessage='';*/
+                }
+            );
+    };
+    self.update = function update(id){
+    	var   List = $resource("http://localhost:8080/updatedonor/:id",{},{save:{method:'PUT',params:{id:'@id'}}});
+    	 //var index = self.DonorsList[index];
+    	 var donors = {};
+    	 donors.donorName = self.selectedContact.donorName;
+    	 donors.donorPhoneNumber=self.selectedContact.donorPhoneNumber;
+    	 donors.donorEmail=self.selectedContact.donorEmail;
+    	/* donors.donorRefName1=self.selectedContact.donorRefName1;
+    	 donors.donorRefName2=self.selectedContact.donorRefName2;*/
+    	 donors.donorRegion=self.selectedContact.donorRegion;
+    	 donors.donorState=self.selectedContact.donorState;
+    	 donors.donorCity=self.selectedContact.donorCity;
+    	 donors.donorcenter=self.selectedContact.donorcenter;
+    	 donors.bankAccountholderName=self.selectedContact.bankAccountholderName;
+    	 donors.bankName=self.selectedContact.bankName;
+    	 donors.bankBranch=self.selectedContact.bankBranch;
+    	 donors.bankAccountNumber=self.selectedContact.bankAccountNumber;
+    	 donors.bankMICR=self.selectedContact.bankMICR;
+    	 donors.bankIfscCode=self.selectedContact.bankIfscCode;
+    	 donors.bankAccountType=self.selectedContact.bankAccountType;
+    	 donors.donationStartDate=self.selectedContact.donationStartDate;	 
+    	 donors.donationEndDate=self.selectedContact.donationEndDate;
+    	 donors.donationFrequency=self.selectedContact.donationFrequency;
+/*    	 donors.createdonordate=self.selectedContact.createdonordate; */
+    	 donors.amount=self.selectedContact.amount;
+    	 donors.donorComments=self.selectedContact.donorComments;
+/*    	 donors.createdonordate = today.getDate();
+  	     donors.creatorName = "sharath"; */
+    	 self.DonorsList[self.selectedContact] = donors;
+    	 List.save({id:self.selectedContact.donorId},donors);
+    	 $log.log("successfully saved"); 
      };
-     self.update = function update(donar_info){
-    	 $log.log(donar_info);
-    	 localService.update(donar_info)
-    	 .then(function(response){
-    		 $log.log(response.data);
-    		 self.DonorsList = response.data;
-    	 },function(erresponse){
-    		 $log.log("Error while upadting"+erresponse);
-    		 
-    	 });
-    	 
-     }
      
 	self.edit = function edit(id){
 		$log.log("The id to be Edited"+id);
 		for(var i=0; i<self.DonorsList.length;i++){
 			if(self.DonorsList[i].consumerCode == id){
-			self.donar_info = angular.copy(self.DonorsList[i]);
+			self.donor_info = angular.copy(self.DonorsList[i]);
 			break;
 			}
 		}
-	}
-	self.selectContact = function(index){
-		self.selectedContact = self.DonorsList[index];
-	}
+	};
+	self.selectContact = function(donorId){
+		console.log('index block');
+		console.log(donorId);
+		self.selectedContact = self.DonorsList[donorId];
+		/*$log.log( self.selectedContact);*/
+		self.StartDate = new Date(self.selectedContact.donationStartDate);
+		self.EndDate = new Date(self.selectedContact.donationEndDate);
+	};
 	
+	self.login=function login(){
+		$log.log(self.logs);
+		localService.login(self.logs)
+		.then(function(d){
+			self.logedUser = d;
+			window.location.href="http://localhost:8080/index.html#!/dashboard";
+		},function(error){
+			$log.log("Error loging in controller");
+			alert("Unable to log-in");
+		});
+		
+	};
+	self.reload = function reload(){
+		$log.log("page is loading");
+		$window.location.reload();
+	};
+	
+	self.userdownload = function(){
+		var downloadPath = 'http://localhost:8080/downloadxls';
+		window.open(downloadPath, '_blank','')
+	};
+	self.monthly_remittances = function(){
+		var downloadPath = 'http://localhost:8080/';
+		window.open(downloadPath, '_blank','')
+	};
+	
+	/*self.file = {};
+	self.uploadFile = function(){
+	console.log(self.file);
+	console.log('service');
+	var uploadurl  = 'http://localhost:8080/uploadxls';
+	fileUpload.post(uploadurl, self.file);
+	}; */
+	
+	$scope.file = {};
+	$scope.uploadfile = function(file) {
+	console.log('$scope.file', $scope.file, file);
+	fileUpload.post(file);
+	} 
 }]);
+
+myApp.directive('fileModel', ['$parse', function ($parse) {
+	console.log('hiiii');
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            var model = $parse(attrs.fileModel);
+            var modelSetter = model.assign;
+            console.log(attrs.fileModel);
+            element.bind('change', function(){
+                scope.$apply(function(){
+                    modelSetter(scope, element[0].files[0]);
+                });
+            });
+        }
+    };
+}]); 
+
+myApp.config(function($routeProvider){
+	$routeProvider
+/*	.when("/",{
+		templateUrl : "login.html"
+	})*/
+	.when("/dashboard",{
+		templateUrl : "dashboard.html"
+	})
+	.when("/donor_info",{
+		templateUrl : "donor_info.html"
+		
+	})
+	.when("/CreateDonor",{
+		templateUrl : "CreateDonor.html"
+	})
+	.when("/ViewDonor",{
+		templateUrl : "ViewDonor.html"
+	})
+	.when("/SuccessPage",{
+		templateUrl : "SuccessPage.html"
+	})
+	.when("/EditDonor",{
+		templateUrl : "EditDonor.html"
+	})
+	.when("/BulkDonor",{
+		templateUrl : "BulkDonor.html"
+	})
+	.when("/Reports",{
+		templateUrl : "Reports.html"
+	})
+});
